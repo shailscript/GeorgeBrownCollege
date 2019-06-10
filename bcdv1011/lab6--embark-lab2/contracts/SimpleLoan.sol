1a0023e780d0b30f38973a625327cdca789fdb89
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 16:09:29 -0400

    Added fucntions to contract

--- bcdv1011/lab6--embark-lab2/contracts/SimpleLoan.sol
+++ bcdv1011/lab6--embark-lab2/contracts/SimpleLoan.sol
@@ -7,9 +7,19 @@ contract SimpleLoan {
     constructor() public {
         owner = msg.sender;
     }
-    
+
     modifier onlyOwner {
         require(owner == msg.sender, "Unauthorized");
         _;
+    }
+
+    function addLoan (address _payee, uint principle, uint rate, uint time) public onlyOwner{
+        uint interest = (principle * rate * time)/100;
+        uint amount = principle+interest;
+        loanRecords[_payee] = amount;
+    }
+
+    function viewLoan (address _payee) public view returns(uint) {
+        return loanRecords[_payee];
     }
 }
