64ec8276d2fd2180d5393e7fcaf9a8f1685f6a38
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 09:04:12 -0400

    Added test for addLoan method

--- bcdv1011/final-project/test/contract_spec.js
+++ bcdv1011/final-project/test/contract_spec.js
@@ -14,4 +14,10 @@ contract('CryptoLoan', () => {
     it("Should be deployed properly", async () => {
         assert.ok(CryptoLoan.options.address);
     });
+
+    it('Should add a loan', async () => {
+        let response = await CryptoLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send();
+        let loan = await CryptoLoan.methods.viewLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07').call();
+        assert.equal(loan, 1040);
+    });
 })
