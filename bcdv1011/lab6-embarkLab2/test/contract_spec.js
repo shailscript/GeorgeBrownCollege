61b0c2a29a4f714d78398ea4b8674a1fd0202097
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-09 23:01:48 -0400

    Cleaned boilerplate code

--- bcdv1011/lab6-embarkLab2/test/contract_spec.js
+++ bcdv1011/lab6-embarkLab2/test/contract_spec.js
@@ -1,10 +1,5 @@
-// /*global contract, config, it, assert*/
-/*
-const SimpleStorage = require('Embark/contracts/SimpleStorage');
-
 let accounts;
 
-// For documentation please see https://embark.status.im/docs/contracts_testing.html
 config({
   //deployment: {
   //  accounts: [
@@ -13,31 +8,7 @@ config({
   //  ]
   //},
   contracts: {
-    "SimpleStorage": {
-      args: [100]
-    }
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
 });
-
-contract("SimpleStorage", function () {
-  this.timeout(0);
-
-  it("should set constructor value", async function () {
-    let result = await SimpleStorage.methods.storedData().call();
-    assert.strictEqual(parseInt(result, 10), 100);
-  });
-
-  it("set storage value", async function () {
-    await SimpleStorage.methods.set(150).send();
-    let result = await SimpleStorage.methods.get().call();
-    assert.strictEqual(parseInt(result, 10), 150);
-  });
-
-  it("should have account with balance", async function() {
-    let balance = await web3.eth.getBalance(accounts[0]);
-    assert.ok(parseInt(balance, 10) > 0);
-  });
-}
-*/
