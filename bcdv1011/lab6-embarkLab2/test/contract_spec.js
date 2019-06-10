ed8cae6f19b35317673821a5a77350f51cdf740d
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-09 23:07:37 -0400

    Registered contract for testing

--- bcdv1011/lab6-embarkLab2/test/contract_spec.js
+++ bcdv1011/lab6-embarkLab2/test/contract_spec.js
@@ -1,14 +1,11 @@
+let SimpleLoan = require('Embark/contracts/SimpleLoan');
+
 let accounts;
 
 config({
-  //deployment: {
-  //  accounts: [
-  //    // you can configure custom accounts with a custom balance
-  //    // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
-  //  ]
-  //},
   contracts: {
-  }
+    SimpleLoan: {},
+  },
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
 });
