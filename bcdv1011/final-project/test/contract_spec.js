67c63482107dcc53b20d888e74a28ec9a5824b1f
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 16:19:24 -0400

    Tests successful

--- bcdv1011/final-project/test/contract_spec.js
+++ bcdv1011/final-project/test/contract_spec.js
@@ -15,15 +15,32 @@ contract('CryptoLoan', () => {
         assert.ok(CryptoLoan.options.address);
     });
 
-    it('Should add a loan', async () => {
-        let response = await CryptoLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send();
+    it('Should add a loan for owner', async () => {
+        let response = await CryptoLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send({from: accounts[0]});
         let loan = await CryptoLoan.methods.viewLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07').call();
         assert.equal(loan, 1040);
     });
 
-    it('Should edit a loan', async () => {
-        let response = await CryptoLoan.methods.editLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 104).send();
+    it('Should edit a loan for owner', async () => {
+        let response = await CryptoLoan.methods.editLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 104).send({from: accounts[0]});
         let loan = await CryptoLoan.methods.viewLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07').call();
         assert.equal(loan, 104);
     });
+
+    it("Should fail when called by others", async () => {
+        try {
+            await CryptoLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send({from: accounts[1]});
+        } catch (error) {
+            assert(error.message.includes("Unauthorized"));
+        }
+    });
+
+    it("Should fail when called by others", async () => {
+        try {
+            await CryptoLoan.methods.editLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 100).send({from: accounts[1]});
+        } catch (error) {
+            assert(error.message.includes("Unauthorized"));
+        }
+    });
+
 });
