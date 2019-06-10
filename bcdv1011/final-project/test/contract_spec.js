b13e0c339dd43327a3ef07048af0315d741ea920
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-09 23:53:24 -0400

    Added first test

--- bcdv1011/final-project/test/contract_spec.js
+++ bcdv1011/final-project/test/contract_spec.js
@@ -9,3 +9,12 @@ config({
 }, (_err, web3_accounts) => {
     accounts = web3_accounts;
 });
+
+contract("CryptoLoan", function() {
+    this.timeout(0);
+
+    it("Should be deployed properly", async function() {
+        console.log(CryptoLoan.options);
+        //assert.ok(CryptoLoan.options.address);
+    });
+});
