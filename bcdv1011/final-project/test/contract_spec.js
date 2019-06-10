9341f792663d773b1c50dee6c967eb7b44964bcd
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 08:12:45 -0400

    Added test for contract deployment

--- bcdv1011/final-project/test/contract_spec.js
+++ bcdv1011/final-project/test/contract_spec.js
@@ -9,3 +9,11 @@ config({
 }, (_err, web3_acccounts) => {
     accounts = web3_acccounts
 });
+
+contract('CryptoLoan', () => {
+    this.timeout(0);
+
+    it("Should be deployed properly", async function() {
+        assert.ok(CryptoLoan.options.address);
+    });
+})
