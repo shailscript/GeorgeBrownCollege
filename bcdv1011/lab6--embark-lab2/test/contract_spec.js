ad51319160a1aa40cf076044f74bd442d143e537
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 16:09:00 -0400

    Added tests for owner restricted functions

--- bcdv1011/lab6--embark-lab2/test/contract_spec.js
+++ bcdv1011/lab6--embark-lab2/test/contract_spec.js
@@ -15,4 +15,17 @@ contract('SimpleLoan', async () => {
         assert.ok(SimpleLoan.options.address);
     });
     
+    it("Should be called by owner", async () => {
+        let response = await SimpleLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send({from: accounts[0]});
+        let loan = await SimpleLoan.methods.viewLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07').call();
+        assert.equal(loan, 1040);
+    });
+
+    it("Should fail when called by others", async () => {
+        try {
+            await SimpleLoan.methods.addLoan('0xed54f39cc9a9cbd9bd4f8fb743769280bbf76b07', 1000, 2, 2).send({from: accounts[1]});
+        } catch (error) {
+            assert(error.message.includes("Unauthorized"));
+        }
+    });
 });
