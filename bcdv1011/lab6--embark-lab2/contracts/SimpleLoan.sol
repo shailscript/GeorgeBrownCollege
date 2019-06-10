7c03aabfabea7c3eac9d4c7cedddc695047bbcba
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-10 16:08:19 -0400

    Added modifier

--- bcdv1011/lab6--embark-lab2/contracts/SimpleLoan.sol
+++ bcdv1011/lab6--embark-lab2/contracts/SimpleLoan.sol
@@ -2,9 +2,14 @@ pragma solidity ^0.5.4;
 
 contract SimpleLoan {
     address owner;
+    mapping (address => uint) loanRecords;
 
     constructor() public {
         owner = msg.sender;
     }
     
+    modifier onlyOwner {
+        require(owner == msg.sender, "Unauthorized");
+        _;
+    }
 }
