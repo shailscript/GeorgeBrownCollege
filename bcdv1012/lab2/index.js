c158c22d4af3e9e8fe6f10f8955f65f39ceeb1b1
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-22 12:01:56 -0400

    Lab 2 completed

--- bcdv1012/lab2/index.js
+++ bcdv1012/lab2/index.js
@@ -1,2 +1,13 @@
 const ethers = require('ethers');
-const utils = require('ethers/utils');
+
+const contractABI = ['function increment(uint256 _counter) public returns(uint256)'];
+
+const contractInterface = new ethers.utils.Interface(contractABI);
+
+const increment = contractInterface.functions.increment;
+
+// Now we encode data to send it to the transaction
+
+const encodedData = increment.encode([2]);
+
+console.log(encodedData);
