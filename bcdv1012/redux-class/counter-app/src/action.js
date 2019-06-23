bd120f70492e39e9d9db5e6744f4ea89054a168a
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-22 21:13:24 -0400

    Redux lecture task removed

--- bcdv1012/redux-class/counter-app/src/action.js
+++
@@ -1,8 +0,0 @@
-export const incrementCount = count => {
-    const incremented = count+1;
-
-    return {
-        type: "INCREMENT_COUNT",
-        count: incremented,
-    }
-}
--- bcdv1012/redux-class/counter-app/src/reducer.js
+++
@@ -1,8 +0,0 @@
-const initialState = {count: 1};
-
-const counterReducer = (state = initialState, action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
-}
