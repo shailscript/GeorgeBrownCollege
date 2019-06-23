eb4ef624fd7af8ad9650857e0a5ab1e0aa4ed421
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-23 16:28:04 -0400

    Deployed contract

--- bcdv1012/lab6/index.js
+++ bcdv1012/lab6/index.js
@@ -7,3 +7,13 @@ require('dotenv').config();
 const provider = ethers.getDefaultProvider('ropsten');
 
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+const factory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+async function deployContract() {
+    const contract = await factory.deploy();
+    console.log(`Contract Info: ${contract}`);
+    console.log(`Contract Address: ${contract.address}`);
+};
+
+deployContract();
