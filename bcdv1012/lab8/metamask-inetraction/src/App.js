04536d89c3166f38edda7eedc0359f2121e07459
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-25 14:25:06 -0400

    Added first basic components

--- bcdv1012/lab8/metamask-inetraction/src/App.js
+++ bcdv1012/lab8/metamask-inetraction/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react';
+import { useWeb3 } from './UseWeb3';
 
-function App() {
+const MetamaskStatus = () => {
+    let w3 = useWeb3();
+    console.log(w3); // look in your JS console
+
   return (
-    <div className="App">
+      <Fragment>
+        <div>status = {w3.status}</div>
+      </Fragment>
+    );
+}
       
-    </div>
-  );
+const App = () => {
+    return <MetamaskStatus />;
 }
 
 export default App;
