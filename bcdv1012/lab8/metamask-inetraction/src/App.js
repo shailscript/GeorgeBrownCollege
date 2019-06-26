8282e12b85ace23b42ddc4d0b2b1fe0acfcf50d4
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-25 23:31:47 -0400

    Minor fixes

--- bcdv1012/lab8/metamask-inetraction/src/App.js
+++ bcdv1012/lab8/metamask-inetraction/src/App.js
@@ -2,32 +2,31 @@ import React from 'react';
 import { useWeb3 } from './UseWeb3';
  
 const MetamaskStatus = () => {
-  let w3 = useWeb3();
-
+  const w3 = useWeb3();
+console.log(w3);
   return (
     <div className='card'>
       <div className='header'>
-        <p>Status = {w3.status}</p>
+        <h2 className='status'>Status: {w3.status}</h2>
         
         { w3.status === 'NO_WEB3' ?
             <a href='https://metamask.io/'>Get metamask</a>
           : (w3.status === 'LOCKED' || w3.status === 'NOT_ENABLED') ?
               <button onClick={w3.enable}>Enable metamask</button>
-            : (w3.account && <UserInfo w3={w3} />)
+            : (w3.account && <UserInfo account={w3.account} />)
         }
       </div>
     </div>
   );
 }
 
-const UserInfo = (props) => {
-  return (
-    <div>
-      <p>{props.w3.account.address}</p>
-      <p>{props.w3.account.networkName}</p>
+const UserInfo = ( { account: { address, networkName} } ) => (
+    <div className='info'>
+      <h2>Wallet information</h2>
+      <p>Address: {address}</p>
+      <p>Network: {networkName}</p>
     </div>
   )
-}
 
 const App = () => {
     return <MetamaskStatus />;
