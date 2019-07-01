4dc3cc27f9820f19e1b9ebb0c93c7b326ea79e8d
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-06-30 20:24:48 -0400

    Added store and provider

--- bcdv1012/lab9/contract-info/src/App.js
+++ bcdv1012/lab9/contract-info/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from './components/Layout';
+import { store} from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div>
+    <Provider store={ store }>
       <Layout />
-    </div>
+    </Provider>
   );
 }
 
