fa3c6e4ee09477d16a722732117f43a041315e84
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-07-02 21:14:46 -0400

    Added config

---
+++ bcdv1012/lec12/frontend/src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    development: {
+        root: "http://localhost:3000",
+        backendUrl: "http://localhost:5000/gbc-firebase-lec/us-central1",
+    }
+}
+
+export default config;
